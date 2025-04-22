b18675012fc0279dadf3ca221a385ec18bf63857
akcineren <erenakcin2002@gmail.com>
2025-04-23 00:01:54 +0300

    Added host ip to allowed hosts

--- application/backend/project/settings.py
+++ application/backend/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--r&m3rfr3av2!+7x%vy32+pv%$r#$du@#mogm&51*0zktjt!1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["134.209.253.215"]
 
 
 # Application definition
