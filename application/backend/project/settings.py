c95e6745018a87f5729a587c52ecf921b58446e6
Umut Şendağ <umutsendag@hotmail.com.tr>
2025-04-23 18:56:44 +0300

    Changed token lifetimes

--- application/backend/project/settings.py
+++ application/backend/project/settings.py
@@ -72,8 +72,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=36500),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=36500),
     "SIGNING_KEY": SECRET_KEY,
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
