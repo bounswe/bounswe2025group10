475797983249d2eb10ab47b0e2d81cc363c66ca6
Umut Şendağ <umutsendag@hotmail.com.tr>
2025-04-25 20:59:44 +0300

    Revert "Update test_settings.py"
    This reverts commit c14bbaa0fae9521fe37c541d979c0ae7cc8bedec.

--- application/backend/project/test_settings.py
+++ application/backend/project/test_settings.py
@@ -3,14 +3,10 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'main_db',
+        'NAME': 'test_db',
         'USER': 'root',
         'PASSWORD': '123456789',
-        'HOST': 'backend-db-1',  
-        'PORT': '3306',       
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'HOST': 'backend-db-1',
+        'PORT': '3306',
     }
 }
