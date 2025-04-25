c8aa90a03db24f07b70312cd1d915a8413f482dd
Umut Şendağ <umutsendag@hotmail.com.tr>
2025-04-25 21:30:11 +0300

    Revert "Change settings.py"
    This reverts commit 1747e233829442cd07653d5d9d4eca1f509804b4.

--- application/backend/project/settings.py
+++ application/backend/project/settings.py
@@ -145,6 +145,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+MIGRATION_MODULES = {
+    'api': None,    # or whatever your app label is
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -154,12 +158,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-import sys
-
-if 'test' in sys.argv:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
