cc2a3027ff1cbb5323b1a90eb5c5a7b277b8e4ea
Mert Kirman <mertkirman@Merts-MacBook-Air-3.local>
2025-05-10 17:35:56 +0300

    Delete function implemented. Public challenges can only be deleted by an admin and non-public challenges can only be deleted by its' creator.

--- application/backend/challenges/urls.py
+++ application/backend/challenges/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ChallengeListCreateView, ChallengeUpdateView
+from .views import ChallengeListCreateView, ChallengeUpdateView, ChallengeDeleteView
 
 urlpatterns = [
     path('', ChallengeListCreateView.as_view(), name='challenge-list-create'),
     path('<int:pk>/update/', ChallengeUpdateView.as_view(), name='challenge-update'),
+    path('<int:pk>/delete/', ChallengeDeleteView.as_view(), name='challenge-delete'),
 ]
--- application/backend/challenges/views.py
+++ application/backend/challenges/views.py
@@ -33,9 +33,9 @@ class ChallengeUpdateView(generics.UpdateAPIView):
     permission_classes = [permissions.IsAuthenticated] # Only authenticated users can update challenges
 
     def update(self, request, *args, **kwargs):
-        """
+        '''
         Custom update logic to restrict updates based on challenge type and user permissions.
-        """
+        '''
         challenge = self.get_object()
         user = request.user
 
@@ -64,4 +64,32 @@ class ChallengeUpdateView(generics.UpdateAPIView):
             
         # Proceed with the update
         return super().update(request, *args, **kwargs)
+    
+
+class ChallengeDeleteView(generics.DestroyAPIView):
+    queryset = Challenge.objects.all()
+    permission_classes = [permissions.IsAuthenticated] # Only authenticated users can delete challenges
+
+    def destroy(self, request, *args, **kwargs):
+        '''
+        Custom delete logic to restrict deletion based on challenge type and user permissions.
+        '''
+        challenge = self.get_object()
+        user = request.user
+
+        if challenge.is_public:
+            # Public challenges can only be deleted by admins
+            if not user.is_staff:
+                return Response(
+                    {"detail": "You do not have permission to delete this public challenge."},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+        else:
+            # Non-public challenges can only be deleted by their creator
+            if challenge.creator != user:
+                return Response(
+                    {"detail": "You do not have permission to delete this private challenge."},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+        return super().destroy(request, *args, **kwargs)
 
