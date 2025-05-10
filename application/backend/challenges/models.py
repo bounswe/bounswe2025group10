033b74b42d19eaba824eb3e0ffe2ad385085db8d
Mert Kirman <mertkirman@Merts-MacBook-Air-3.local>
2025-05-10 15:22:05 +0300

    Set permissions for reading and creating challenges. Public challenges can be read by everyone, non-public challenges can only be read by their creators. Each authenticated user can create public/non-public challenges.

--- application/backend/challenges/models.py
+++ application/backend/challenges/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from api.models import Achievements, Users
 
+
 class Challenge(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     target_amount = models.FloatField(blank=True, null=True)
     current_progress = models.FloatField(blank=True, null=True)
-    is_public = models.IntegerField(blank=True, null=True)
+    is_public = models.BooleanField(default=False)
     reward = models.ForeignKey(Achievements, models.DO_NOTHING, blank=True, null=True)
+    creator = models.ForeignKey(Users, models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
         db_table = 'Challenge'
--- application/backend/challenges/views.py
+++ application/backend/challenges/views.py
@@ -1,7 +1,26 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Challenge
 from .serializers import ChallengeSerializer
 
 class ChallengeListCreateView(generics.ListCreateAPIView):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        '''
+        Customize the queryset to show public challenges to everyone and non-public challenges only to their creators.
+        '''
+        user = self.request.user
+        if user.is_authenticated:
+            # Authorized users can see their own challenges and public challenges
+            return Challenge.objects.filter(is_public=True) | Challenge.objects.filter(creator=user)
+        else:
+            # Unauthenticated users can only see public challenges
+            return Challenge.objects.filter(is_public=True)
+        
+    def perform_create(self, serializer):
+        '''
+        Set the creator of the challenge to the current user when creating a new challenge.
+        '''
+        serializer.save(creator=self.request.user)
