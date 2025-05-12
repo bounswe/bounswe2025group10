e657d23950284c8f15a13219fcd399b9d6e8b776
Mert Kirman <mertkirman@Merts-MacBook-Air-3.local>
2025-05-12 19:16:49 +0300

    Allow user to participate in public and private challenges.

--- application/backend/challenges/urls.py
+++ application/backend/challenges/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ChallengeListCreateView, ChallengeUpdateView, ChallengeDeleteView
+from .views import ChallengeListCreateView, ChallengeUpdateView, ChallengeDeleteView, ChallengeParticipationView
 
 urlpatterns = [
     path('', ChallengeListCreateView.as_view(), name='challenge-list-create'),
     path('<int:pk>/update/', ChallengeUpdateView.as_view(), name='challenge-update'),
     path('<int:pk>/delete/', ChallengeDeleteView.as_view(), name='challenge-delete'),
+    path('participate/', ChallengeParticipationView.as_view(), name='challenge-participation'),
 ]
--- application/backend/challenges/views.py
+++ application/backend/challenges/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from .models import Challenge
-from .serializers import ChallengeSerializer
+from .models import Challenge, UserChallenge
+from .serializers import ChallengeSerializer, ChallengeParticipationSerializer
 
 class ChallengeListCreateView(generics.ListCreateAPIView):
     queryset = Challenge.objects.all()
@@ -92,4 +92,15 @@ class ChallengeDeleteView(generics.DestroyAPIView):
                     status=status.HTTP_403_FORBIDDEN
                 )
         return super().destroy(request, *args, **kwargs)
+    
+
+class ChallengeParticipationView(generics.CreateAPIView):
+    queryset = UserChallenge.objects.all()
+    serializer_class = ChallengeParticipationSerializer
+    permission_classes = [permissions.IsAuthenticated] # Only authenticated users can participate in challenges
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
 
