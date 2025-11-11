43c4f463e318e21263e3c4f0190b36ac5d712e4f
Ceyda Irwin <ceyda.irwin@gmail.com>
2025-11-11 16:47:47 +0300

    Add comprehensive unit tests for mobile app

--- application/mobile/jest.config.js
+++ application/mobile/jest.config.js
@@ -3,6 +3,28 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|expo|@expo|expo-.*)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|expo|@expo|expo-.*|axios)/)',
   ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.{ts,tsx}',
+    '!**/__tests__/**',
+    '!**/__mocks__/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.{ts,tsx}',
+    '**/*.test.{ts,tsx}',
+  ],
+  moduleNameMapper: {
+    '^@env$': '<rootDir>/__mocks__/env.js',
+  },
 };
