182ba1ce02cbd9fe6a53536dfa282421683f2735
Umut Şendağ <umutsendag@hotmail.com.tr>
2025-12-09 16:16:53 +0300

    small fix for temp solution

--- archive/application/backend/project/settings.py
+++ archive/application/backend/project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--r&m3rfr3av2!+7x%vy32+pv%$r#$du@#mogm&51*0zktjt!1p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["134.209.253.215", "localhost", '134-209-253-215.sslip.io', '127.0.0.1', '10.0.2.2', '165.22.20.242']
+ALLOWED_HOSTS = ["134.209.253.215", "localhost", '134-209-253-215.sslip.io', '127.0.0.1', '10.0.2.2']
 
 
 # Application definition
